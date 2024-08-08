eaef9aa59ded49947295efac76eafa508b42a4ac
Syah Warid Ghani Akram <github@tira.my.id>
2024-08-08 19:26:32 +0700

    feat: toggle bot indicator

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,3 +2,5 @@ import { parseEnvValue } from "./functions/parseEnvValue.js";
 
 export const channelsId: string[] = parseEnvValue(process.env.CHANNELS_ID ?? "");
 export const webhooksUrl: string[] = parseEnvValue(process.env.WEBHOOKS_URL ?? "");
+
+export const enableBotIndicator: boolean = process.env.ENABLE_BOT_INDICATOR?.toLowerCase() === "yes";
